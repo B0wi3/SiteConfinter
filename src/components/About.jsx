8d9048d2e4ad5b2a6ff9f6c433e84fab912157f5
b0wi3 <94808901+B0wi3@users.noreply.github.com>
2024-03-15 18:19:49 +0000

    Remove "about" section image, work on responsiveness with less dependance on media queries

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,22 +10,13 @@ function About() {
   return (
     <div id="about-frame">
       <div id="about">
-        <h1 id="title" style={{ marginBottom: "20px" }}>
-          QUEM SOMOS NÓS
-        </h1>
+        <h1 id="title">QUEM SOMOS NÓS</h1>
         <div id="buttons">
           <button className="btn">Missão</button>
           <button className="btn">Visão</button>
           <button className="btn">Valores</button>
         </div>
         {cardInfo.map(createCard)}
-      </div>
-      <div id="img-container">
-        <img
-          className="img-about"
-          src="https://emeritus.org/in/wp-content/uploads/sites/3/2022/03/What-is-Financial-Management.jpg.webp"
-          alt="about-complement"
-        />
       </div>
     </div>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,35 +2,33 @@ import React from "react";
 
 function Contact() {
   return (
-    <div>
-      <h1 className="contact-header">Fale com a gente</h1>
-      <div id="contact">
-        <form id="form">
-          <div className="form-row">
-            <div className="form-element">
-              <label for="fname">Primeiro nome</label>
-              <input type="text" id="fname" />
-            </div>
-            <div className="form-element">
-              <label for="lname">Sobrenome</label>
-              <input type="text" id="lname" />
-            </div>
+    <div id="contact">
+      <h1>Fale com a gente</h1>
+      <form id="form">
+        <div className="form-row">
+          <div className="form-element">
+            <label for="fname">Primeiro nome</label>
+            <input type="text" id="fname" />
           </div>
-          <div className="form-row">
-            <div className="form-element">
-              <label for="email">Email</label>
-              <input type="email" id="email" />
-            </div>
+          <div className="form-element">
+            <label for="lname">Sobrenome</label>
+            <input type="text" id="lname" />
           </div>
-          <div className="form-row">
-            <div className="form-element">
-              <label for="message">Mensagem</label>
-              <textarea type="text" id="message" />
-            </div>
+        </div>
+        <div className="form-row">
+          <div className="form-element">
+            <label for="email">Email</label>
+            <input type="email" id="email" />
           </div>
-          <input type="submit" value="Enviar" id="submit-form" />
-        </form>
-      </div>
+        </div>
+        <div className="form-row">
+          <div className="form-element">
+            <label for="message">Mensagem</label>
+            <textarea type="text" id="message" />
+          </div>
+        </div>
+        <input type="submit" value="Enviar" id="submit-form" />
+      </form>
     </div>
   );
 }
